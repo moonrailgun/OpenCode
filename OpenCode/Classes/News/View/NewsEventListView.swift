a27d1b99d52f25737d96d236a32200016b57a97b
moonrailgun <moonrailgun@gmail.com>
2016-05-29 13:51:19 +0800

    列表框架与数据获取
    ADD 列表框架与数据获取

--- OpenCode/Classes/News/View/NewsEventListView.swift
+++ OpenCode/Classes/News/View/NewsEventListView.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class NewsEventListView: UIView {
+class NewsEventListView: UIView, UITableViewDataSource, UITableViewDelegate {
 
     /*
     // Only override drawRect: if you perform custom drawing.
@@ -18,16 +19,68 @@ class NewsEventListView: UIView {
     }
     */
     
+    let NEWS_EVENT_CELL_ID = "newsEvent"
+    var tableView:UITableView?
+    var dataArr:JSON?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        /*
         let label = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 40))
         label.text = "画面"
-        self.addSubview(label)
+        self.addSubview(label)*/
+        initView()
+        initData()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    func initView(){
+        self.tableView = UITableView(frame: self.bounds, style: .Plain)
+        tableView?.dataSource = self
+        tableView?.delegate = self
+        self.addSubview(tableView!)
+    }
+    func initData(){
+        Github.getEvents { (data:AnyObject?) -> Void in
+            let json = JSON(data!)
+            self.dataArr = json
+            print("github事件 数据加载完毕")
+            if json.count > 0{
+                OperationQueueHelper.operateInMainQueue({ () -> Void in
+                    //todo 刷新列表数据
+                    self.tableView!.reloadData()
+                })
+            }
+        }
+    }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let d = dataArr{
+            return d.count
+        }else{
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(NEWS_EVENT_CELL_ID)
+        
+        if(cell == nil){
+            cell = UITableViewCell(style: .Default, reuseIdentifier: NEWS_EVENT_CELL_ID)
+        }
+        
+        return cell!
+    }
 }
