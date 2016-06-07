6206788e752b2a85cf2d81de4df7c34577015338
moonrailgun <moonrailgun@gmail.com>
2016-06-07 12:53:10 +0800

    提交详情框架
    ADD 提交详情框架

---
+++ OpenCode/Classes/CommitDetail/Controller/CommitDetailController.swift
@@ -0,0 +1,37 @@
+//
+//  CommitDetailController.swift
+//  OpenCode
+//
+//  Created by 陈亮 on 16/6/7.
+//  Copyright © 2016年 moonrailgun. All rights reserved.
+//
+
+import UIKit
+
+class CommitDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
