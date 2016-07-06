defad20a8151f591741f72e23201e8536d915a98
moonrailgun <moonrailgun@gmail.com>
2016-07-06 21:24:43 +0800

    ping工具界面UI
    ADD ping工具界面UI

--- OpenCode/Classes/Tool/Ping/Controller/PingController.swift
+++ OpenCode/Classes/Tool/Ping/Controller/PingController.swift
@@ -10,15 +10,35 @@ import UIKit
 
 class PingController: UIViewController {
 
+    lazy var pingHostAddress:UITextField = UITextField(frame: CGRectMake(20,20 + 64,self.view.frame.width - 40,40))
+    lazy var pingStartButton:UIButton = UIButton(type: UIButtonType.System)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.title = "Ping工具"
+        
+        pingHostAddress.borderStyle = .RoundedRect
+        pingHostAddress.placeholder="请输入地址"
+        pingHostAddress.textAlignment = .Center
+        pingHostAddress.contentVerticalAlignment = .Center
+        pingHostAddress.contentHorizontalAlignment = .Center
+        pingHostAddress.clearButtonMode = .WhileEditing
+        self.view.addSubview(pingHostAddress)
+        pingStartButton.frame = CGRectMake(20,60 + 64,self.view.frame.width - 40, 40)
+        pingStartButton.setTitle("开始ping", forState: .Normal)
+        pingStartButton.addTarget(self, action: #selector(self.startPing), forControlEvents: .TouchUpInside)
+        self.view.addSubview(pingStartButton)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startPing(){
     }
     
 
