c06abb07895c0c10957bec82bf0faf08ee40b400
moonrailgun <moonrailgun@gmail.com>
2016-08-04 13:50:28 +0800

    ping操作与结果计算
    ADD ping操作与结果计算

--- OpenCode/Classes/Tool/Ping/Controller/PingDetailController.swift
+++ OpenCode/Classes/Tool/Ping/Controller/PingDetailController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class PingDetailController: UIViewController {
     var hostAddress:String?
+    var timer:NSTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: #selector(self.ping), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        if(timer != nil){
+            startPing()
+        }
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        if(timer != nil){
+            stopPing()
+        }
+    }
+    
+    func startPing(){
+        self.timer?.fire()
+    }
+    
+    func stopPing(){
+        self.timer?.invalidate()
+    }
+    
+    func ping() {
+        print("a")
+        if let address = hostAddress{
+            SimplePingHelper.start(address, target: self, selector: #selector(self.pingResult(_:)))
+        }
+    }
+    
+    func pingResult(object:AnyObject){
+        let result = JSON(object)
+        if(result["status"] == true){
+            //ping到
+            print(result["time"])
+        }else{
+            //没有
+            print(result["error"])
+        }
     }
     
     
